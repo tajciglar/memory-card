76ef48c9bacec0a6f949e9ceb3d197e8f24426aa
tajciglar <taj.brzovic@gmail.com>
2024-05-01 12:26:34 +0200

    Add function to shuffle cards and logic if user clicks the same one again

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react';
+import '../styles/cards.css';
 
 
 export default function Cards() {
     const [cardImages, setCardImages] = useState([]);
+    const [clickedCards, setClickedCards] = useState([]);
+    const [wrongClick, setWrongClick] = useState(false);
 
     useEffect(() => {
         async function getCardImages(){
             try {
                 const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php');
                 const data = await response.json();
-                console.log(data)
                 const images = data.data
                 .filter(card => card.type === "Effect Monster")
                 .map(card => card.card_images[0].image_url_small)
                 .slice(0, 8);
-                console.log('image url:', images)
                 setCardImages(images);
             } catch (error) {
                 console.log('Error: ', error);
@@ -23,13 +24,42 @@ export default function Cards() {
         getCardImages();
     }, []);
 
-    return (
-        <div>
+    const handleCardClick = (event) => {
+        const clickedCard = event.target;
+        console.log(clickedCard.src);
+        if (!clickedCards.includes(clickedCard.src)) {
+            setClickedCards(prevClickedCards => [...prevClickedCards, clickedCard.src]);
+            shuffleCards();
+        } else {
+            alert("wrong")
+            setWrongClick(true);
+        }
+        
+    }
+
+    function shuffleCards() {
+        for (let i = cardImages.length - 1; i > 0; i--) {
+            const randomIndex = Math.floor(Math.random() * (i + 1));
+            [cardImages[i], cardImages[randomIndex]] = [cardImages[randomIndex], cardImages[i]];
+        }
+    }
+
+    const cardsContent = (
+        <div className='cardsContainer'>
             {cardImages.map((imageUrl, index) => (
                 <div key={index}>
-                   <img src={imageUrl} alt={`Effect Monster ${index}`} />
+                    <img src={imageUrl} alt={`Effect Monster ${index}`} id={index} onClick={handleCardClick} />
                 </div>
             ))}
         </div>
     );
+
+    // JSX content for when wrongClick is true
+    const errorMessage = (
+        <div>
+            {/* Add your error message JSX here */}
+        </div>
+    );
+
+    return wrongClick ? errorMessage : cardsContent;
 }
